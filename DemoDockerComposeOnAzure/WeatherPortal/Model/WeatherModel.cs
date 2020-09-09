ac02db936837c6365f0fb79ceb9cae2b4dd9ce96
Adnan Samuel <yesadahmed@gmail.com>
2020-09-09 11:33:14 +0200

    Dockercompose portal and api demo

---
+++ DemoDockerComposeOnAzure/WeatherPortal/Model/WeatherModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WeatherPortal.Model
+{
+    public class WeatherModel
+    {
+        public List<WeatherData> Data { get; set; }
+    }
+
+    public class WeatherData
+    {
+        public DateTime date { get; set; }
+        public int temperatureC { get; set; }
+        public int temperatureF { get; set; }
+        public string summary { get; set; }
+    }
+
+}
---
+++ DemoDockerComposeOnAzure/WeatherPortal/Pages/Index.cshtml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using WeatherPortal.Model;
+
+namespace WeatherPortal.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task OnGet()
+        {
+            ViewData["Message"] = "Weather Report";
+
+            using (var client = new System.Net.Http.HttpClient())
+            {
+                // Call *mywebapi*, and display its response in the page
+                var request = new System.Net.Http.HttpRequestMessage();
+                request.RequestUri = new Uri("http://weatherapi/WeatherForecast"); // ASP.NET 3 (VS 2019 only)
+                //request.RequestUri = new Uri("http://mywebapi/api/values/1"); // ASP.NET 2.x
+                var response = await client.SendAsync(request);
+
+
+
+
+                var josn = await response.Content.ReadAsStringAsync();
+                var data = JsonConvert.DeserializeObject<List<WeatherModel>>(josn);
+
+                ViewData["Message"] = JsonConvert.SerializeObject(await response.Content.ReadAsStringAsync());
+                ViewData["MsgJson"] = JsonConvert.SerializeObject(data);
+
+
+            }
+        }
+    }
+}
