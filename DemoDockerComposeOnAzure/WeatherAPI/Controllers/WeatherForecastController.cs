ec1a2b38dc72584f4bcbbd303fae480265572c76
Adnan Samuel <yesadahmed@gmail.com>
2020-10-10 13:30:16 +0200

    changes to reflect devops pipeline container deployments

--- DemoDockerComposeOnAzure/WeatherAPI/Controllers/WeatherForecastController.cs
+++ DemoDockerComposeOnAzure/WeatherAPI/Controllers/WeatherForecastController.cs
@@ -24,7 +24,8 @@ namespace WeatherAPI.Controllers
         [HttpGet]
         public List<DataEx> Get()
         {
-            string lat = "59.913868"; string lon = "10.752245";
+           // string lat = "59.913868"; string lon = "10.752245"; //oslo
+            string lat = "51.538320"; string lon = "-0.136910"; //london (camden town)
             WeatherOperations weatherOperations = new WeatherOperations();
             return weatherOperations.GetWeatherDataOslo(lat, lon);
         }
